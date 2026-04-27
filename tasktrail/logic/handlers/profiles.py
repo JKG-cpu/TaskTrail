9ec0afba90d37824174690d2a4f03a07735d53f4
JKG <jkg66512@gmail.com>
2026-04-27 18:04:28 +0000

    Added + Testing grabbing class data => gonna start showing it + adding it with the TUI

--- tasktrail/logic/handlers/profiles.py
+++ tasktrail/logic/handlers/profiles.py
@@ -1,4 +1,5 @@
 from ..helpers import *
+from .classes import ClassHandler
 
 class ProfileHandler:
     def __init__(self, data: dict) -> None:
@@ -7,14 +8,20 @@ class ProfileHandler:
         self.current_user = None
         self.user_data = None
 
+        self.class_handler = ClassHandler()
+
     # Helpers
     def _grab_data(self, username: str) -> dict: return self.data.get(username)
-    def _construct_variables(self) -> None: self.user_data = self._grab_data(self.current_user)
+    def _construct_variables(self) -> None: 
+        self.user_data = self._grab_data(self.current_user)
+        self.class_handler.set_data(self.user_data["classes"])
 
     # Callables
-    def sign_out(self) -> None: self.current_user, self.user_data = None, None
     def get_user(self) -> str: return self.current_user if self.current_user != None else "Guest"
-    
+    def sign_out(self) -> None: 
+        self.current_user, self.user_data = None, None
+        self.class_handler.set_data(None)
+
     def log_in(self, username: str, password: str) -> int: 
         data = self.data.get(username, None)
 
--- testing.py
+++ testing.py
@@ -1,1 +1,6 @@
 from tasktrail.logic import *
+import pprint
+
+s = Services()
+s.profile_handler.log_in("Profile 1", "password")
+pprint.pprint(s.profile_handler.class_handler.get_assignments())
