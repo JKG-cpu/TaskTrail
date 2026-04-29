42fad58c751edc52793e66c44642c21bd6db039d
JKG <jkg66512@gmail.com>
2026-04-29 20:12:21 +0000

    Added some styling to the settings page from base.tcss => Move onto class adding + removing and profiles

--- tasktrail/widgets/pages/settingspage.py
+++ tasktrail/widgets/pages/settingspage.py
@@ -10,9 +10,9 @@ class SettingsPage(Static):
 
     def compose(self) -> ComposeResult:
         if self.username:
-            yield Label(f"Hello, {self.username}")
+            yield Label(f"Hello, {self.username}", classes = "title")
             yield Button("Sign out", classes = "signOut")
 
         else:
-            yield Label("You are not logged in!")
+            yield Label("You are not logged in!", classes = "title")
             yield Button("Login", classes = "login")
